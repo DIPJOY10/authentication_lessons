d2383359a44b17ca0dfa2289c0b057b28808428c
unknown <basakdipjoy@gmail.com>
2020-08-11 13:16:09 +0530

    add files for authentication

---
+++ app.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+require('dotenv').config();
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+const ejs=require('ejs');
+const encrypt=require('mongoose-encryption');
+
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/secretDB",{useNewUrlParser:true,useUnifiedTopology:true});
+
+const accountSchema=new mongoose.Schema({
+  username:String,
+  password:String
+});
+//encryption using mongoose-encryption package
+accountSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
+const User=new mongoose.model("User",accountSchema);
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+//level one security using basic registration
+app.post("/register",function(req,res){
+  const user=new User({
+    username:req.body.username,
+    password:req.body.password
+  });
+  user.save(function(err){
+    if(err)
+    console.log(err);
+    else
+    res.render("secrets");
+  });
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+app.post("/login",function(req,res){
+  User.findOne({username:req.body.username},function(err,foundUser){
+    if(foundUser){
+      if(foundUser.password===req.body.password)
+      res.render("secrets");
+      else
+      res.send("wrong password");
+    }
+    else
+    res.send("no matching email found");
+  });
+});
+app.get("/",function(req,res){
+  res.render("home");
+});
+app.listen(3000,function(){
+  console.log("Server running on port 3000");
+});
